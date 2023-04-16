53a1895038e0c6d32ce18565ef342be4c7a125d0
Detravoir <thomasdiondehaas@hotmail.com>
2023-04-16 17:01:28 +0200

    front end

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ import { VegaTree } from "./libraries/vegatree.js";
 function addEventListenerToSubmitButton() {
     let mass_multiplier = document.getElementById("mass_multiplier");
     let radius_multiplier = document.getElementById("radius_multiplier");
-    let eccentricity = document.getElementById("eccentricity");
+    let orbital_radius = document.getElementById("orbital_radius");
     let submitBtn = document.getElementById("submit-button");
   
     submitBtn.addEventListener("click", (e) => {
       e.preventDefault();
-      if (!mass_multiplier.value || !radius_multiplier.value || !eccentricity.value) {
+      if (!mass_multiplier.value || !radius_multiplier.value || !orbital_radius.value) {
         alert("Please fill out all fields");
       } else {
         let planet = {
         mass_multiplier: mass_multiplier.value, 
         radius_multiplier: radius_multiplier.value, 
-        eccentricity: eccentricity.value
+        eccentricity: orbital_radius.value
       };
       loadSavedModel(planet);
       }
@@ -33,27 +33,28 @@ function addEventListenerToSubmitButton() {
       });
   }
   
-  function modelLoaded(model, planet) {
+  function modelLoaded(model, patient) {
     let decisionTree = new DecisionTree(model);
-    let prediction = decisionTree.predict({
-      mass_multiplier: planet.mass_multiplier,
-      radius_multiplier: planet.radius_multiplier,
-      eccentricity: planet.eccentricity
-    });
-    console.log("predicted: " + prediction);
-  
+    let prediction = decisionTree.predict(patient);
+
     let userInput = document.getElementById("userInput");
+    let resultImage = document.getElementById("resultImage");
+    
     let value;
     if(prediction == 'Gas Giant'){
-      value = 'It is a Gass Giant';
-    } if (prediction == 'Super Earth') {
+      value = 'It is a Gas Giant';
+      resultImage.src = "src/gas-giant.jpg"; // Replace with the actual image filename
+    } else if (prediction == 'Super Earth') {
       value = 'This is a Super Earth';
-    } if (prediction == 'Neptune-like'){
+      resultImage.src = "src/super-earth.jpg"; // Replace with the actual image filename
+    } else if (prediction == 'Neptune-like'){
       value = 'This is a Neptune Like planet';
+      resultImage.src = "src/neptune-like.jpg"; // Replace with the actual image filename
     }
+    
     if (userInput !== null && prediction !== undefined) {
-        userInput.innerText = `the computer thinks that ${value}`;
-      }
+        userInput.innerText = `The computer thinks that ${value}`;
+    }
       
     let visual = new VegaTree("#view", 900, 500, decisionTree.toJSON());
   }
